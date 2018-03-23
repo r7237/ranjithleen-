b6575a2fc8c59342f8175481af708a4804e39607
r7237 <35914754+r7237@users.noreply.github.com>
2018-03-23 15:53:15 +0530

    Create 22225555255525.c

---
+++ 22225555255525.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,a[100],n=25,t;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(a[j]<a[j+1])
+            {
+                t=a[j];
+                a[j]=a[j+1];
+                a[j+1]=t;
+            }
+        }
+    }
+    printf("%d",a[0]);
+    return 0;
+}
+    
